aebff7454c77525dea81dabf43ab61c4d2e344ee
Alexandros Stefanidis <44155972+astefanidis1@users.noreply.github.com>
2026-08-01 11:24:01 -0400

    Load World Completion Pass I module

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 (()=>{
-  const modules=['01-core.js','02-records.js','02a-density.js','02b-ecology-a.js','02b-ecology-b.js','02b-ecology-c.js','02b-ecology-d.js','02c-continuity.js','02d-assets.js','02e-assurance.js','02f-release-candidate.js','03-interface.js'];
+  const modules=['01-core.js','02-records.js','02a-density.js','02b-ecology-a.js','02b-ecology-b.js','02b-ecology-c.js','02b-ecology-d.js','02c-continuity.js','02d-assets.js','02e-assurance.js','02f-release-candidate.js','02g-world-completion.js','03-interface.js'];
   const roots=['source/','../src/app/'];
 
   async function loadRoot(root){
